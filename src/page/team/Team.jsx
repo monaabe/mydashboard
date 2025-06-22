6abab1146904325e021d2805838047f64e6b766d
monaabe <monaabed484@gmail.com>
2025-06-22 17:07:01 +0300

    my dashboard

--- src/page/team/Team.jsx
+++ src/page/team/Team.jsx
@@ -1,11 +1,106 @@
-import React from 'react'
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { rows } from "./data";
+import { Typography, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 
- const Team=()=> {
+
+const Team = () => {
+  const theme = useTheme();
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      align: "center",
+      headerAlign: "center",flex:1
+
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      align: "center",
+      headerAlign: "center",flex:1
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      align: "center",
+      headerAlign: "center",flex:1
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      align: "center",
+      headerAlign: "center",flex:1
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      align: "center",
+
+      headerAlign: "center",flex:1
+    },
+    {
+      field: "access",
+      headerName: "Access",
+      align: "center",
+      headerAlign: "center",flex:1.9,
+      // @ts-ignore
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            sx={{
+              backgroundColor:
+                access === "admin"
+                  ? theme.palette.primary.dark
+                  : access === "manager"
+                  ? theme.palette.secondary.dark
+                  : "#3da58a",
+
+              p: "5px",
+              width: "99px",
+              borderRadius: "3px",
+              textAlign: "center",
+              mx: "auto",
+              display: "flex",
+              justifyContent: "space-evenly",
+            }}
+          >
+            {access === "admin" && (
+              <AdminPanelSettingsOutlinedIcon fontSize="small" />
+            )}
+
+                        {access === "manager" && (
+              <SecurityOutlinedIcon fontSize="small" />
+            )}
+
+            {access === "user" && (
+              <LockOpenOutlinedIcon   fontSize="small" />
+              
+            )}
+
+            <Typography sx={{ fontSize: "13px", fontWeight: "bold" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
   return (
-    <div>
-      team pagee
-    </div>
-  )
-}
-   
+    <Box sx={{ height: 300, width: '98%', mx:"auto" }}>
+      <DataGrid 
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </Box>
+  );
+};
+
 export default Team;
---
+++ src/page/team/data.jsx
@@ -0,0 +1,56 @@
+
+export const rows = [
+  {
+    "id": 1,
+    "name": "Ali Hassan",
+    "email": "ali@gmail.com",
+    "phone": "01012345678",
+    "age": 28,
+    "access": "admin"
+  },
+  {
+    "id": 2,
+    "name": "Sara Adel",
+    "email": "sara@gmail.com",
+    "phone": "01087654321",
+    "age": 24,
+    "access": "user"
+  },
+  {
+    "id": 3,
+    "name": "Mostafa Khaled",
+    "email": "mostafa@gmail.com",
+    "phone": "01123456789",
+    "age": 35,
+    "access": "user"
+  },
+    {
+    "id": 4,
+    "name": "samar Khaled",
+    "email": "samar@gmail.com",
+    "phone": "01123456789",
+    "age": 35,
+    "access": "manager"
+  },
+    {
+    "id": 5,
+    "name": "amera Khaled",
+    "email": "amera@gmail.com",
+    "phone": "01123456789",
+    "age": 35,
+    "access": "manager"
+  },
+    {
+    "id": 6,
+    "name": "Mona Abed",
+    "email": "mona@gmail.com",
+    "phone": "01123456789",
+    "age": 35,
+    "access": "manager"
+  }
+
+
+
+]
+
+
